2eb3c6a5d7d1f1bcd704b540b72a4baf67eac3d5
Muhammad-Talha103 <talhagp908@gmail.com>
2025-02-01 15:31:09 +0500

    update

--- components/Cart/Checkout/Checkout.tsx
+++ components/Cart/Checkout/Checkout.tsx
@@ -88,7 +88,7 @@ const BillingDetails = () => {
   ];
 
   const validateForm = () => {
-    let errors: FormErrors = {};
+    const errors: FormErrors = {};
     billingForm.forEach((item) => {
       if (item.required && !formData[item.field as keyof FormData]) {
         errors[item.field as keyof FormData] = `${item.title} is required`;
