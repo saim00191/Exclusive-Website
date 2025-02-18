c355988dee0c5aafdde0dc814a4efbf684b2a5c9
Muhammad-Talha103 <talhagp908@gmail.com>
2025-02-18 16:53:50 +0500

    update

--- firebase/firebase.tsx
+++ firebase/firebase.tsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqy6ocM7OZ2CKBWngQxsxVBIX_rJzhonI",
-  authDomain: "exclusive-1722f.firebaseapp.com",
-  projectId: "exclusive-1722f",
-  storageBucket: "exclusive-1722f.firebasestorage.app",
-  messagingSenderId: "581399016104",
-  appId: "1:581399016104:web:e4b342472893c01d84889d",
-  measurementId: "G-MV18XM6QMX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);
